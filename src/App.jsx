8289469fa8187bd59cfbb9e4f135373a3a73aae1
Ashish <grunch.me1@gmail.com>
2024-11-06 13:23:46 +0545

    dashboard path is made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import AppLayout from "./AppLayout";
+import Dashboard from "./Dashboard";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
