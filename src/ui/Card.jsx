c9820b22126ab7f90ddebb19e4f73ba409462ef5
Ashish <grunch.me1@gmail.com>
2024-11-15 00:25:48 +0545

    dashboad report card is made

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import "./card.css";
 
-function Card() {
-    return <div>
-        <section className="card-body">
-            <h5>Customer</h5>
-            <img src="./public/group.png"/>
+function Card({ title, iconPath, countIcon, count, reportNum }) {
+  return (
+    <>
+      <section className="card-body">
+        <div className="card-header">
+          <h4>{title}</h4>
+
+          <figure className="card-icon">
+            <img src={iconPath} alt={title} />
+          </figure>
+        </div>
+        <h4 className= "card-count" ><i className={countIcon}/>{count}</h4>
+        <p className="card-report">
+          <span>{reportNum}</span> Since last month
+        </p>
       </section>
-  </div>;
+    </>
+  );
 }
 
 export default Card;
--- src/ui/Dashboard.jsx
+++ src/ui/Dashboard.jsx
@@ -1,22 +1,60 @@
 import Calender from "./Calender";
 import Card from "./card";
-import "./dashboard.css"
+import "./dashboard.css";
 
 function Dashboard() {
   return (
-    <div>
+    <div className="container">
       <section className="dash__header">
         <h4>Dashboard</h4>
         <div>
           <Calender />
-          <span className="pi pi-calendar "></span>
-          <span className="pi pi-cart-plus "></span>
-          <span className="pi pi-sync "></span>
+          <span className="pi pi-calendar pi-icon "></span>
+          <span className="pi pi-cart-plus pi-icon"></span>
+          <span className="pi pi-sync pi-icon"></span>
         </div>
       </section>
       <section className="dash__body">
         <div className="card__body">
-        <Card />
+          <Card
+            title="customer"
+            iconPath="./public/group.png"
+            count="0"
+            reportNum="0%"
+          />
+          <Card
+            title="Booking"
+            iconPath="./public/cart.png"
+            count="0"
+            reportNum="⬇50%"
+          />
+          <Card
+            title="Grocery Store"
+            iconPath="./public/group.png"
+            count="25"
+          />
+        </div>
+        <div className="card__body">
+          <Card
+            title="Total Revenue"
+            iconPath="./public/dollar-symbol.png"
+            countIcon="pi pi-indian-rupee"
+            count="0"
+            reportNum="⬇64.74%"
+          />
+          <Card
+            title="Total Sold Product"
+            iconPath="./public/dollar-symbol.png"
+            count="0"
+          />
+          <Card
+            title="Products"
+            iconPath="./public/trending.png"
+            countIcon="pi pi-plus"
+            count="0"
+            reportNum="⬇64.74%"
+          />
+          <Card title="Manager" iconPath="./public/group.png" count="0" />
         </div>
       </section>
     </div>
